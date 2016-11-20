d41fdb7299b71b7a9a9df46dd4b7584a48348be3
Lukaka4331 <105021044@live.asia.edu.tw>
2016-11-20 23:36:52 +0800

    m

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -11,21 +11,19 @@ public class ex01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn=new Scanner(System.in);
-        int a =scn.nextInt();
-        int c=0;
-	int data[]=new int[a];
-	System.out.println(var(a));}
-	public static int var(int x,int y){
-	         
-	
-	
-	
-	
-	
-	
-	
-	
-	   }
-	}
-
-
+        int n =scn.nextInt();
+        float data[]=new float [n];
+        int sum=0;
+        for(int i=0;i<n;i++){
+        	 data[i]=scn.nextInt();
+        	 sum+=data[i];
+        }sum/=n;
+        System.out.println(var(sum,data)/n);
+        	}
+public static float var(float sum,float data[]){
+	float total=0;
+	for(int i=0;i<data.length;i++){
+		total+=(sum-data[i])*(sum-data[i]);
+	}return sum;
+}
+}
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,5 +1,5 @@
 package ex;
-
+import java.util.Scanner;
 /*
  * Date: 2016/11/14
  * Author: 1050210XX OOO
@@ -9,6 +9,27 @@ public class ex02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		float data[]=new float[n];
+		float sum=0;
+		float all=0;
+		for(int i=0;i<n;i++){
+			 data[i]=scn.nextInt();
+        	 sum+=data[i];
+		}sum/=n;
+		for(int i=0;i<n;i++){
+			all+= var(sum,data[i]);
+		}        System.out.println(std(all/n));}
+	public static float var(float sum,float data){
+		float total=0;
+		total+=(sum-data)*(sum-data);
+		return total;}
+		public static double std(double fin){
+			fin=Math.pow(fin,1/2);return fin;
+		}
+		
+		
 	}
 
-}
+
